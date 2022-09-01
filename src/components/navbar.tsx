12452ae6ad631960c0e91347aa6513cbec687005
daveclinton <clintondavid46@gmail.com>
2022-09-01 21:30:30 +0300

    ft: Add elements to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,19 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
 import React from "react";
 
 function navbar() {
-  return <Flex>Hello World</Flex>;
+  return (
+    <Flex flexDir="row" gap="100px">
+      <Box>
+        <Link>LOGO</Link>
+      </Box>
+      <Flex flexDir="row">
+        <Link>Home</Link>
+        <Link>Portfolio</Link>
+        <Button>CALL-ME</Button>
+      </Flex>
+    </Flex>
+  );
 }
 
 export default navbar;
