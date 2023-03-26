0e7d97e1b95c23e10761a5b23591287ee01e21f1
daveclinton <clintondavid46@gmail.com>
2023-03-26 08:22:36 +0300

    Feat: Add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 
-export const App = () => (
-  <ChakraProvider>
-    <Navbar />
-  </ChakraProvider>
-);
+const App: React.FC = () => {
+  return (
+    <ChakraProvider>
+      <Navbar />
+    </ChakraProvider>
+  );
+};
+
+export default App;
